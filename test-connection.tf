9f347a111bb433a627dc32c82682b1a8db37d2f9
Julien Acroute <julien.acroute@camptocamp.com>
2022-06-29 17:17:49 +0200

    Add container to connect to the replica

--- test-connection.tf
+++ test-connection.tf
@@ -22,6 +22,23 @@ resource "docker_container" "psql" {
   ]
 }
 
+# Create a container
+resource "docker_container" "psql-replica" {
+  image      = docker_image.postgres.latest
+  name       = "psql-replica"
+  dns        = ["8.8.8.8"]
+  tty        = true
+  entrypoint = ["/bin/bash"]
+  command    = []
+  env = [
+    "PGHOST=replica-${local.pg_infos.host}",
+    "PGPORT=${local.pg_infos.port}",
+    "PGPASSWORD=${local.pg_infos.password}",
+    "PGUSER=${local.pg_infos.username}",
+    "PGDATABASE=${local.pg_infos.database}",
+  ]
+}
+
 resource "local_sensitive_file" "foo" {
   content  = "${local.pg_infos.host}:${local.pg_infos.port}:${local.pg_infos.database}:${local.pg_infos.username}:${local.pg_infos.password}"
   filename = pathexpand("~/.pgpass")
